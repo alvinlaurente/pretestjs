40cf18d62a4373c6d36c99fa0aabb0054571c9f7
Alvin <alvin.ertanto@gmail.com>
2020-10-07 22:26:51 +0700

    update werewolf

--- Latihan OOP/werewolf.js
+++ Latihan OOP/werewolf.js
@@ -5,13 +5,13 @@ class Time {
 }
 
 class Moderator {
-  setTime(name, time) {
-    if (time === "day") {
-      name.time = time;
-      return `Current time is ${name.time}`;
-    } else if (time === "night") {
-      name.time = time;
-      return `Current time is ${name.time}`;
+  setTime(time, cycle) {
+    if (cycle === "day") {
+      time.time = cycle;
+      return `Current time is ${time.time}`;
+    } else if (cycle === "night") {
+      time.time = cycle;
+      return `Current time is ${time.time}`;
     } else {
       return `Time inputed is wrong`;
     }
@@ -64,7 +64,7 @@ class Guardian extends Player {
     if (time.time === "night") {
       if (name.alive === true) {
         name.guarded = true;
-        return `${name.name} is guarded this night!`;
+        return `${name.name} is guarded tonight!`;
       } else {
         return `${name.name} is already dead.`;
       }
@@ -131,44 +131,6 @@ class Werewolf extends Player {
   }
 }
 
-/*
-function countPlayer() {
-  return players.length;
-}
-
-function countVillageAlive() {
-  let counter = 0;
-  for (let i = 0; i < players.length; i++) {
-    if (players[i].team === "village" && players[i].alive === true) {
-      counter++;
-    }
-  }
-
-  return counter;
-}
-
-function countWerewolfAlive() {
-  let counter = 0;
-  for (let i = 0; i < players.length; i++) {
-    if (players[i].team === "werewolf" && players[i].alive === true) {
-      counter++;
-    }
-  }
-
-  return counter;
-}
-
-function winner() {
-  if (countWerewolfAlive() == 0) {
-    return `Villager is the winner`;
-  } else if (countWerewolfAlive() == countVillageAlive()) {
-    return `Werewolf is the winner`;
-  } else {
-    return `Game still continue, finish the game first!`;
-  }
-}
-*/
-
 let time = new Time("day");
 let mod = new Moderator();
 let alvin = new Villager("Alvin");
@@ -176,30 +138,29 @@ let bayu = new Villager("Bayu");
 let cindy = new Villager("Cindy");
 let dody = new Guardian("Dody");
 let eria = new Seer("Eria");
-let fira = new Werewolf("Fira");
+let fajar = new Werewolf("Fajar");
 
-// let players = [alvin, bayu, cindy, dody, eria, fira];
+// let players = [alvin, bayu, cindy, dody, eria, fajar];
 
 // Perintah
 console.log(time);
 
-console.log(dody.guard(time, alvin)); // Gagal waktu masih siang
-console.log(eria.see(time, dody)); // Gagal waktu masih siang
-console.log(fira.kill(time, alvin)); // Gagal waktu masih siang
-console.log(mod.setTime(time, "night")); // Set waktu jadi malam
+console.log(dody.guard(time, alvin)); // Current time still day! Guard can only be done in night
+console.log(eria.see(time, dody)); // Current time still day! See can only be done in night
+console.log(fajar.kill(time, alvin)); // Current time still day! Kill can only be done in night
+console.log(mod.setTime(time, "night")); // Current time is night
 console.log(time);
 
-console.log(dody.guard(time, alvin)); // Berhasil melindungi
-console.log(eria.see(time, dody)); // Berhasil melihat
-console.log(fira.kill(time, alvin)); // Gagal Membunuh
-console.log(alvin.setUnguarded(time)); // Gagal waktu masih malam
-console.log(mod.setTime(time, "day")); // Set waktu jadi siang
+console.log(dody.guard(time, alvin)); // Alvin is guarded tonight!
+console.log(eria.see(time, dody)); // Dody's team is village
+console.log(fajar.kill(time, alvin)); // You failed to kill Alvin. Alvin is guarded!
+console.log(alvin.setUnguarded(time)); // Current time still night! This activity can only be done in daytime!
+console.log(mod.setTime(time, "day")); // Current time is day
 console.log(time);
 
-console.log(alvin.setUnguarded(time)); // Berhasil mengubah status guard
-console.log(mod.setTime(time, "night"));
-console.log(fira.kill(time, alvin));
-console.log(fira.kill(time, bayu));
-console.log(fira.kill(time, cindy));
-console.log(fira.kill(time, dody));
-console.log(mod.setTime(time, "day"));
+console.log(alvin.setUnguarded(time)); // Alvin is unguarded!
+console.log(mod.setTime(time, "night")); // Current time is night
+console.log(dody.guard(time, dody)); // Dody is guarded tonight!
+console.log(eria.see(time, fajar)); // Fajar's team is werewolf
+console.log(fajar.kill(time, alvin)); // Alvin is dead
+console.log(mod.setTime(time, "day")); // Current time is day
